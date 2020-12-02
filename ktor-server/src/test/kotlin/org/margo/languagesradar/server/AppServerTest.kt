0a2f5cfe4bbeaf9ee0e85e2c48ba29b74400145a
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2020-12-02 23:30:35 +0100

    Refactor tests

--- ktor-server/src/test/kotlin/org/margo/languagesradar/server/AppServerTest.kt
+++ ktor-server/src/test/kotlin/org/margo/languagesradar/server/AppServerTest.kt
@@ -23,12 +23,14 @@ class AppServerTest {
         withTestApplication(Application::main) {
             with(handleRequest(HttpMethod.Get, "/latest?langs=Java")) {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("{\n" +
-                        "  \"JAVA\" : {\n" +
-                        "    \"version\" : \"15\",\n" +
-                        "    \"notes\" : [ \"oracle.com/java/technologies/javase/15u-relnotes.html\" ]\n" +
-                        "  }\n" +
-                        "}", response.content)
+                assertEquals("""
+                    {
+                      "JAVA" : {
+                        "version" : "15",
+                        "notes" : [ "oracle.com/java/technologies/javase/15u-relnotes.html" ]
+                      }
+                    }
+                """.trimIndent(), response.content)
             }
         }
     }
@@ -37,34 +39,36 @@ class AppServerTest {
         withTestApplication(Application::main) {
             with(handleRequest(HttpMethod.Get, "/latest")) {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("{\n" +
-                        "  \"JAVA\" : {\n" +
-                        "    \"version\" : \"15\",\n" +
-                        "    \"notes\" : [ \"oracle.com/java/technologies/javase/15u-relnotes.html\" ]\n" +
-                        "  },\n" +
-                        "  \"KOTLIN\" : {\n" +
-                        "    \"version\" : \"1.4.20\",\n" +
-                        "    \"notes\" : [ \"kotlinlang.org/releases.html\" ]\n" +
-                        "  },\n" +
-                        "  \"SCALA\" : {\n" +
-                        "    \"version\" : \"2.13.4\",\n" +
-                        "    \"notes\" : [ \"github.com/scala/scala/releases/tag/v2.13.4\" ]\n" +
-                        "  },\n" +
-                        "  \"GO\" : {\n" +
-                        "    \"version\" : \"1.15\",\n" +
-                        "    \"notes\" : [ \"golang.org/doc/go1.15\" ]\n" +
-                        "  },\n" +
-                        "  \"RUBY\" : {\n" +
-                        "    \"version\" : \"2.7.2\",\n" +
-                        "    \"notes\" : [ \"www.ruby-lang.org/en/news/2020/10/02/ruby-2-7-2-released/\" ]\n" +
-                        "  },\n" +
-                        "  \"APEX\" : null,\n" +
-                        "  \"SWIFT\" : {\n" +
-                        "    \"version\" : \"5.3.1\",\n" +
-                        "    \"notes\" : [ \"https://github.com/apple/swift/releases/tag/swift-5.3.1-RELEASE\" ]\n" +
-                        "  },\n" +
-                        "  \"DOTTY\" : null\n" +
-                        "}", response.content)
+                assertEquals("""
+                    {
+                      "JAVA" : {
+                        "version" : "15",
+                        "notes" : [ "oracle.com/java/technologies/javase/15u-relnotes.html" ]
+                      },
+                      "KOTLIN" : {
+                        "version" : "1.4.20",
+                        "notes" : [ "kotlinlang.org/releases.html" ]
+                      },
+                      "SCALA" : {
+                        "version" : "2.13.4",
+                        "notes" : [ "github.com/scala/scala/releases/tag/v2.13.4" ]
+                      },
+                      "GO" : {
+                        "version" : "1.15",
+                        "notes" : [ "golang.org/doc/go1.15" ]
+                      },
+                      "RUBY" : {
+                        "version" : "2.7.2",
+                        "notes" : [ "www.ruby-lang.org/en/news/2020/10/02/ruby-2-7-2-released/" ]
+                      },
+                      "APEX" : null,
+                      "SWIFT" : {
+                        "version" : "5.3.1",
+                        "notes" : [ "https://github.com/apple/swift/releases/tag/swift-5.3.1-RELEASE" ]
+                      },
+                      "DOTTY" : null
+                    }
+                """.trimIndent(), response.content)
             }
         }
     }

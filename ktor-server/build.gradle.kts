87d7fb451e83669932a8e3439a92de0613762f2c
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2021-05-07 14:13:37 +0200

    Use Kotlin 1.5.0

--- ktor-server/build.gradle.kts
+++ ktor-server/build.gradle.kts
@@ -8,23 +8,23 @@ repositories {
 }
 
 jacoco {
-    toolVersion = "0.8.6"
+    toolVersion = "0.8.7"
     reportsDir = file("$buildDir/reports/jacoco")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.ktor:ktor-server-core:1.4.0")
-    implementation("io.ktor:ktor-server-netty:1.4.0")
-    implementation("io.ktor:ktor-jackson:1.4.0")
-    implementation("io.ktor:ktor-html-builder:1.4.3")
+    implementation("io.ktor:ktor-server-core:1.5.4")
+    implementation("io.ktor:ktor-server-netty:1.5.4")
+    implementation("io.ktor:ktor-jackson:1.5.4")
+    implementation("io.ktor:ktor-html-builder:1.5.4")
 
     implementation(project(":scrapper"))
     implementation(project(":html-extender"))
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.willowtreeapps.assertk:assertk:0.23")
-    testImplementation("io.ktor:ktor-server-test-host:1.4.3")
+    testImplementation("io.ktor:ktor-server-test-host:1.5.4")
 }
 
 application {
